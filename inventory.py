fa403518d1f592f9ac97a059135ef7952ce77618
Tristenx <1604tristenarron@gmail.com>
2025-07-05 00:00:07 +0100

    uses self.filepath instead of requiring arg

--- inventory.py
+++ inventory.py
@@ -7,48 +7,50 @@ class Inventory:
 
     def __init__(self):
         self.filepath = "inventory_data.json"
-        if not self.json_exists(self.filepath):
-            self.create_json_file(self.filepath)
+        if not self.json_exists():
+            self.create_json_file()
 
-    def json_exists(self, filepath: str) -> bool:
+    def json_exists(self) -> bool:
         """Checks if a file exists."""
         try:
-            with open(filepath, "r", encoding="utf-8"):
+            with open(self.filepath, "r", encoding="utf-8"):
                 return True
         except FileNotFoundError:
             return False
 
-    def create_json_file(self, filepath: str):
+    def create_json_file(self):
         """Creates a json file."""
         default_data = {
             "item": {
                 "price": 0
             }
         }
-        with open(filepath, "w", encoding="utf-8") as file:
+        with open(self.filepath, "w", encoding="utf-8") as file:
             json.dump(default_data, file, indent=4)
 
-    def add_item(self, filepath: str, item: str, price: float):
+    def add_item(self, item: str, price: float):
         """Adds an item and its price to the json file."""
-        new_item_data = {
-            item: {
-                "price": price
+        if isinstance(price, (float, int)):
+            new_item_data = {
+                item: {
+                    "price": price
+                }
             }
-        }
-        with open(filepath, "r", encoding="utf-8") as file:
-            data = json.load(file)
-        data.update(new_item_data)
-        with open(filepath, "w", encoding="utf-8") as file:
-            json.dump(data, file, indent=4)
+            with open(self.filepath, "r", encoding="utf-8") as file:
+                data = json.load(file)
+            data.update(new_item_data)
+            with open(self.filepath, "w", encoding="utf-8") as file:
+                json.dump(data, file, indent=4)
 
-    def remove_item(self, filepath: str, item: str):
+    def remove_item(self, item: str):
         """Removes an item from the json file."""
-        with open(filepath, "r", encoding="utf-8") as file:
+        with open(self.filepath, "r", encoding="utf-8") as file:
             data = json.load(file)
-        data.pop(item)
-        with open(filepath, "w", encoding="utf-8") as file:
-            json.dump(data, file, indent=4)
+        if item in data:
+            data.pop(item)
+            with open(self.filepath, "w", encoding="utf-8") as file:
+                json.dump(data, file, indent=4)
 
 
 inv = Inventory()
-inv.add_item(inv.filepath, "bread", 2.7)
+inv.add_item("coffee", 4.70)
